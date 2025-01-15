1fb93e4963b05a262347df573f23a09e9143df32
9Hogo9onyix <168650849+9Hogo9onyix@users.noreply.github.com>
2025-01-15 04:46:19 -0800

    transform+js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("hi")
+var boxjs = document.getElementsByClassName('box')[0]
+console.log(boxjs)
+function clickonmove(){
+    // alert('you click on move')
+   boxjs.style.backgroundColor='red'
+   boxjs.style.color='blue'
+   boxjs.innerHTML="this from js"
+   boxjs.style.fontSize= '40px'
+   boxjs.style.justifyConten='center'
+  
+}
