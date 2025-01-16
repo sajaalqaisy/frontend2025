c8051692dec609e1649b645cb14b29e1ea72ad50
sajaalqaisy <osaja8304@gmail.com>
2025-01-16 21:06:53 +0300

    transition

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var boxjs = document.getElementsByClassName('box')[0]
 console.log(boxjs)
 function clickonmove(){
     // alert('you click on move')
-   boxjs.style.backgroundColor='red'
+   boxjs.style.backgroundColor='black'
    boxjs.style.color='blue'
    boxjs.innerHTML="this from js"
    boxjs.style.fontSize= '40px'
